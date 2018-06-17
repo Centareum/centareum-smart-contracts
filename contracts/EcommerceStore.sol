d9b641fa36c8e073ab9be696128e8a1471b42bc8
kanthgithub <kanth.github@gmail.com>
2018-06-17 09:35:56 +0800

    remove obselete auction feature in ECommerceStore

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -18,28 +18,23 @@ contract EcommerceStore {
         uint startPrice;
         ProductStatus status;
         ProductCondition condition;
-        mapping(address => mapping(bytes32 => Bid)) bids;
-
     }
 
     function EcommerceStore() public {
         productIndex = 0;
     }
 
-    function addProductToStore(string _name, string _category, string _imageLink, string _descLink, uint _auctionStartTime,
-        uint _auctionEndTime, uint _startPrice, uint _productCondition) public {
-        require(_auctionStartTime < _auctionEndTime);
+    function addProductToStore(string _name, string _category, string _imageLink, string _descLink,  uint _startPrice, uint _productCondition) public {
         productIndex += 1;
-        Product memory product = Product(productIndex, _name, _category, _imageLink, _descLink, _auctionStartTime, _auctionEndTime,
+        Product memory product = Product(productIndex, _name, _category, _imageLink, _descLink,
             _startPrice, 0, 0, 0, 0, ProductStatus.Open, ProductCondition(_productCondition));
         stores[msg.sender][productIndex] = product;
         productIdInStore[productIndex] = msg.sender;
     }
 
-    function getProduct(uint _productId) view public returns (uint, string, string, string, string, uint, uint, uint, ProductStatus, ProductCondition) {
+    function getProduct(uint _productId) view public returns (uint, string, string, string, string, uint,  ProductStatus, ProductCondition) {
         Product memory product = stores[productIdInStore[_productId]][_productId];
-        return (product.id, product.name, product.category, product.imageLink, product.descLink, product.auctionStartTime,
-        product.auctionEndTime, product.startPrice, product.status, product.condition);
+        return (product.id, product.name, product.category, product.imageLink, product.descLink,  product.startPrice, product.status, product.condition);
     }
 
 }

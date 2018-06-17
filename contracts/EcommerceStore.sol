fd0631715e4e4110e6a75d98ae9da38d643a0223
kanthgithub <kanth.github@gmail.com>
2018-06-17 09:45:18 +0800

    Code Documentation added for contract

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -1,14 +1,27 @@
 pragma solidity ^0.4.13;
 
-
+//Contract to publish/maintain Products in Marketplace
 contract EcommerceStore {
+
+    //enum to hold valid set for ProductStatus
     enum ProductStatus {Open, Sold, Unsold}
+
+    //enum to hold vaild set for ProductCondition
     enum ProductCondition {New, Used}
 
+    //Product Index - equivalent to a Sequence in Database.
     uint public productIndex;
+
+    //Map to maintain ProductOwner and his/her Products published in store
+    //Key: Address of ProductOwner , Value: ProductMap of the ProductOwner
+    //(Product-Map contains all Products published by Owner in order of productIndex)
     mapping(address => mapping(uint => Product)) stores;
+
+    //Map to maintain owner/publisher of the product and product
+    //Key: ProductIndex , Value: address of ProductOwner
     mapping(uint => address) productIdInStore;
 
+    //Struct for Product
     struct Product {
         uint id;
         string name;
@@ -20,11 +33,13 @@ contract EcommerceStore {
         ProductCondition condition;
     }
 
+    //constructor
     function EcommerceStore() public {
         productIndex = 0;
     }
 
-    function addProductToStore(string _name, string _category, string _imageLink, string _descLink,  uint _startPrice, uint _productCondition) public {
+    //add/Publish product to Store.
+    function addProductToStore(string _name, string _category, string _imageLink, string _descLink, uint _startPrice, uint _productCondition) public {
         productIndex += 1;
         Product memory product = Product(productIndex, _name, _category, _imageLink, _descLink,
             _startPrice, 0, 0, 0, 0, ProductStatus.Open, ProductCondition(_productCondition));
@@ -32,9 +47,11 @@ contract EcommerceStore {
         productIdInStore[productIndex] = msg.sender;
     }
 
-    function getProduct(uint _productId) view public returns (uint, string, string, string, string, uint,  ProductStatus, ProductCondition) {
+    //extract product Data-Struct by ProductIndex
+    //Function does a lookup operation on Map for stores
+    function getProduct(uint _productId) view public returns (uint, string, string, string, string, uint, ProductStatus, ProductCondition) {
         Product memory product = stores[productIdInStore[_productId]][_productId];
-        return (product.id, product.name, product.category, product.imageLink, product.descLink,  product.startPrice, product.status, product.condition);
+        return (product.id, product.name, product.category, product.imageLink, product.descLink, product.startPrice, product.status, product.condition);
     }
 
 }
